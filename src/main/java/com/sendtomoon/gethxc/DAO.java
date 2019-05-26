cfe2a3a4c0cc3a659c4f69d73e7f802b5702e666
galaxysha512 <galaxysha512@gmail.com>
2019-05-26 16:26:23 +0800

    fix

--- src/main/java/com/sendtomoon/gethxc/DAO.java
+++ src/main/java/com/sendtomoon/gethxc/DAO.java
@@ -2,8 +2,6 @@ package com.sendtomoon.gethxc;
 
 import java.util.List;
 
-import org.mybatis.spring.support.SqlSessionDaoSupport;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import com.sendtomoon.gethxc.dto.GetListByTagRespDataDTO;
@@ -15,17 +13,5 @@ public interface DAO {
 	public List<GetListByTagRespDataDTO> getWaitDown();
 
 	public int update(GetListByTagRespDataDTO dto);
-
-//	public int add(GetListByTagRespDataDTO dto) {
-//		return getSqlSession().insert("hxc.video.add", dto);
-//	}
-//
-//	public List<GetListByTagRespDataDTO> getWaitDown() {
-//		return getSqlSession().selectList("hxc.video.getWaitDown", null);
-//	}
-//
-//	public int update(GetListByTagRespDataDTO dto) {
-//		return getSqlSession().update("hxc.video.update", dto);
-//	}
 
 }
